f8b72768cc71d1912a267f0ea5dc0ffb1faabfdd
Alejandro <stevenarenas74@gmail.com>
2025-07-21 21:58:46 -0500

    Agregue los tipos de variables y la impresion de mensajes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        //TIPOS DE VARIABLES
+
+        //Numeros Enteros
+        byte variableByte = 100;
+        short variableShort = 1000;
+        int variableInt = 10000;
+        long variableLong = 100000l;
+
+        //Numero Reales
+        float variableFloat = 1.5555555f;
+        double variableDouble = 1.55555555;
+
+        //Booleanos
+        boolean variableBoolean = true;
+
+        //Cadenas y Caracteres
+        String variableString = "Hola mundo";
+        char variableChar = 'A';
+
+        //Mostrar Valores
+
+        System.out.println("Variable Byte=" + variableByte);
+        System.out.println("Variable Short: " + variableShort);
+        System.out.println("Variable Int: " + variableInt);
+        System.out.println("Variable Long: " + variableLong);
+
+        System.out.println("Variable float: " + variableFloat);
+        System.out.println("Variable Double: "+ variableDouble);
+
+        System.out.println("Variable Booleanos :" + variableBoolean);
+
+        System.out.println("Variable String:" + variableString);
+        System.out.println("Variable Char: " + variableChar);
+
     }
+
 }
